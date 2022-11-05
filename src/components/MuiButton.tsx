5fbbf80f6181983cbdba6a5707162c02555275e5
Aaveg Gupta <aaveggupta.contact@gmail.com>
2022-11-05 15:52:09 +0530

    button toggle

--- src/components/MuiButton.tsx
+++ src/components/MuiButton.tsx
@@ -1,8 +1,31 @@
 import React from "react";
-import { Stack, Button, IconButton, ButtonGroup } from "@mui/material";
+import {
+  Stack,
+  Button,
+  IconButton,
+  ButtonGroup,
+  ToggleButton,
+  ToggleButtonGroup,
+} from "@mui/material";
 import SendIcon from "@mui/icons-material/Send";
+import FormatBoldIcon from "@mui/icons-material/FormatBold";
+import FormatItalicIcon from "@mui/icons-material/FormatItalic";
+import FormatUnderlinedIcon from "@mui/icons-material/FormatUnderlined";
+
+import { useState } from "react";
 
 const MuiButton = () => {
+  const [formats, setFormats] = useState<string | null>(null);
+
+  console.log(formats);
+
+  const handleFormatChange = (
+    _event: React.MouseEvent<HTMLElement>,
+    updatedFormats: string | null
+  ) => {
+    setFormats(updatedFormats);
+  };
+
   return (
     // STACK IS JUST LIKE DISPLAY FLEX
     <Stack spacing={4}>
@@ -75,6 +98,25 @@ const MuiButton = () => {
           <Button>Update</Button>
           <Button>Delete</Button>
         </ButtonGroup>
+      </Stack>
+
+      <Stack direction="row">
+        <ToggleButtonGroup
+          aria-label="text-formatting"
+          value={formats}
+          onChange={handleFormatChange}
+          exclusive
+        >
+          <ToggleButton value="bold" aria-label="bold">
+            <FormatBoldIcon />
+          </ToggleButton>
+          <ToggleButton value="italic" aria-label="italic">
+            <FormatItalicIcon />
+          </ToggleButton>
+          <ToggleButton value="underlined" aria-label="underlined">
+            <FormatUnderlinedIcon />
+          </ToggleButton>
+        </ToggleButtonGroup>
       </Stack>
     </Stack>
   );
